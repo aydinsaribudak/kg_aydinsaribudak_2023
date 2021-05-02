b053fd94d2d97102cc4948d6d9c36ad105faa345
aydinsaribudak <aydin.saribudak@gmail.com>
2021-05-02 01:03:45 -0400

    print phonetic equivalent, seperate with commas

--- main.py
+++ main.py
@@ -19,9 +19,38 @@ for i in range(len(a)):
         rem = quot % 10
         remainder.append(rem)
         quot = int(quot / 10)
-        print(quot)
+        #print(quot)
+    
+    #print(remainder)
+    for j in reversed(remainder):
+        if j == 1:
+            print("One", end="")
+        elif j == 2:
+            print("Two", end="")
+        elif j == 3:
+            print("Three", end="")
+        elif j == 4:
+            print("Four", end="")
+        elif j == 5:
+            print("Five", end="")
+        elif j == 6:
+            print("Six", end="")
+        elif j == 7:
+            print("Seven", end="")
+        elif j == 8:
+            print("Eight", end="")
+        elif j == 9:
+            print("Nine", end="")
+        elif j == 0:
+            print("Zero", end="")
+        else:
+            print("NA", end="")
 
-    print(remainder)
+    if i < len(a)-1:
+        print(",", end="")
+    else:
+        print("")
+
 
 
 
