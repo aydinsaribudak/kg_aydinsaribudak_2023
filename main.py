8b87bd741e9cbdb664531a60f502ffea470aaa09
Aydin Saribudak <aydins@js-17-185.jetstream-cloud.org>
2021-05-02 00:35:07 -0500

    read input from console

--- main.py
+++ main.py
@@ -1,57 +1,64 @@
-#import numpy as np
+import sys#, imp, json, traceback, time, socket, math
 
-
-
-a = [34, 2, 467]
-
-for i in range(len(a)):
-
-    remainder = []
-    
-    # calcute remainder and quotient
-    rem = a[i] % 10
-    remainder.append(rem)
-    quot = int(a[i] / 10)
-    #print(quot)
-
-    # calcute remainder and quotient recursively
-    while quot >= 1:
-        rem = quot % 10
-        remainder.append(rem)
-        quot = int(quot / 10)
-        #print(quot)
-    
-    #print(remainder)
-    for j in reversed(remainder):
-        if j == 1:
-            print("One", end="")
-        elif j == 2:
-            print("Two", end="")
-        elif j == 3:
-            print("Three", end="")
-        elif j == 4:
-            print("Four", end="")
-        elif j == 5:
-            print("Five", end="")
-        elif j == 6:
-            print("Six", end="")
-        elif j == 7:
-            print("Seven", end="")
-        elif j == 8:
-            print("Eight", end="")
-        elif j == 9:
-            print("Nine", end="")
-        elif j == 0:
-            print("Zero", end="")
-        else:
-            print("NA", end="")
-
-    if i < len(a)-1:
-        print(",", end="")
+try: 
+    if len(sys.argv) > 1:
+        inputList = sys.argv[1:]
     else:
-        print("")
+        print("Missing input")
+        print("Please enter your command in this format: python main.py 23 34")
+        sys.exit(0)
 
+    for i in range(len(inputList)):
+    
+        remainder = []
+        
+        # calcute remainder and quotient
+        inputListInt = int(inputList[i])
+        rem = inputListInt % 10
+        #print(rem)
+        remainder.append(rem)
+        quot = int(inputListInt / 10)
+        #print(quot)
 
+        # calcute remainder and quotient recursively
+        while quot >= 1:
+            rem = quot % 10
+            remainder.append(rem)
+            quot = int(quot / 10)
+            #print(quot)
+    
+        #print(remainder)
+        for j in reversed(remainder):
+            if j == 1:
+                print("One", end="")
+            elif j == 2:
+                print("Two", end="")
+            elif j == 3:
+                print("Three", end="")
+            elif j == 4:
+                print("Four", end="")
+            elif j == 5:
+                print("Five", end="")
+            elif j == 6:
+                print("Six", end="")
+            elif j == 7:
+                print("Seven", end="")
+            elif j == 8:
+                print("Eight", end="")
+            elif j == 9:
+                print("Nine", end="")
+            elif j == 0:
+                print("Zero", end="")
+            else:
+                print("NA", end="")
+
+        if i < len(inputList)-1:
+            print(",", end="")
+        else:
+            print("")
+
+except Exception as ex:
+    print("error")
 
 
 
